30e3b9163a39dda905635f8d9b4b05ee724e6045
Haitian Li <44156794+HaitianLi@users.noreply.github.com>
2021-03-14 18:37:52 +0800

    0314

--- 20210205_BitC_字符串函数/inCLass.c
+++ 20210205_BitC_字符串函数/inCLass.c
@@ -2,16 +2,8 @@
  * @Author: Haitian Li
  * @Date: 2021-03-12 13:35:52
  * @LastEditors: Haitian Li
- * @LastEditTime: 2021-03-12 18:29:52
+ * @LastEditTime: 2021-03-14 09:17:43
  * @Description: I do not wish to be horny anymore,
  *               I just want to be happy.
  */
 
-#include <stdio.h>
-
-
-int main() {
-    
-
-    return 0;
-}
--- 20210312_BitC_/inClass.c
+++ 20210312_BitC_/inClass.c
@@ -2,7 +2,44 @@
  * @Author: Haitian Li
  * @Date: 2021-03-12 18:29:24
  * @LastEditors: Haitian Li
- * @LastEditTime: 2021-03-12 18:29:24
+ * @LastEditTime: 2021-03-14 10:47:20
  * @Description: I do not wish to be horny anymore,
  *               I just want to be happy.
  */
+
+#include <stdio.h>
+#include <errno.h>
+
+
+
+int main(int argc, char ** argv) {
+    
+    // FILE * pf = fopen("./data.txt","w");
+    
+    // // if (pf == NULL) {
+    // //     printf("%s\n", stderror(errno));
+    // //     return 1;
+    // // }
+    // fputs("hello world!\n", pf);
+
+    // fclose(pf);
+    // pf = NULL;
+
+
+    FILE * pf = fopen("./data.txt", "r");
+    
+
+    char * buffer_stdin;
+    char buffer_file_stream[1];
+
+    fgets(buffer_stdin,4,stdin);
+    fgets(buffer_file_stream, 4, pf);
+    
+    printf("%s\n%s\n", buffer_stdin, buffer_file_stream);
+
+    fclose(pf);
+    
+
+        
+    return 0;
+}
